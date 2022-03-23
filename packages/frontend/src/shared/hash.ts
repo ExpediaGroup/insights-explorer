b24593896b31497fc7c680928ad8e6b72d000630
Dave Bauman <dbauman@expediagroup.com>
2022-03-22 23:51:50 -0400

    refactor(lint): Enable `unicorn/prefer-code-point` rule in frontend

--- packages/frontend/src/shared/hash.ts
+++ packages/frontend/src/shared/hash.ts
@@ -17,11 +17,13 @@
 export const hashCode = (str: string): number => {
   let hash = 0,
     i: number,
-    chr: number;
+    chr: number | undefined;
   for (i = 0; i < str.length; i++) {
-    chr = str.charCodeAt(i);
-    hash = (hash << 5) - hash + chr;
-    hash |= 0;
+    chr = str.codePointAt(i);
+    if (chr !== undefined) {
+      hash = (hash << 5) - hash + chr;
+      hash |= 0;
+    }
   }
   return hash;
 };
