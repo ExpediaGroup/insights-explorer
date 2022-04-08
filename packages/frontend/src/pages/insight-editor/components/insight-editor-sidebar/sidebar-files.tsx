7925e1b8f33e274166dc29959fb5ad4208bfc9b1
Dave Bauman <dave@davebauman.io>
2022-04-08 17:03:46 -0400

    fix: Make the Insight Editor file sidebar open initially
    Ideally, the file sidebar would be closed initially on mobile, and open initially on desktop.  Unfortunately, `useBreakpointValue` defaults to `base`, so the page appears to be a mobile page on the first render.  So a conditional like this doesn't work:
    
    ```
    const { isOpen: isFilesOpen, onToggle: onFilesToggle } = useDisclosure({ defaultIsOpen: !isMobile });
    ```
    
    Instead of trying to workaround this, just changing it to default to open, as this is not an unreasonable default even on mobile.  We can revisit it later if necessary.

--- packages/frontend/src/pages/insight-editor/components/insight-editor-sidebar/sidebar-files.tsx
+++ packages/frontend/src/pages/insight-editor/components/insight-editor-sidebar/sidebar-files.tsx
@@ -178,7 +178,7 @@ export const SidebarFiles = ({
 
   const isMobile = useBreakpointValue({ base: true, md: false });
 
-  const { isOpen: isFilesOpen, onToggle: onFilesToggle } = useDisclosure({ defaultIsOpen: false });
+  const { isOpen: isFilesOpen, onToggle: onFilesToggle } = useDisclosure({ defaultIsOpen: true });
 
   const iconOption = useBreakpointValue({
     base: isFilesOpen ? iconFactoryAs('chevronUp') : iconFactoryAs('chevronDown'),
